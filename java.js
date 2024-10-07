444d471a0a73ccf19e84b91f4d11f75bceca35e3
guilherme <106540089+guilherme21577@users.noreply.github.com>
2024-10-07 12:43:11 -0300

    kkkkkkkkkkkk

--- java.js
+++ java.js
@@ -7,15 +7,16 @@ let tentativa = 1;
 while(chute != numeroSecreto){
     chute = prompt("que tipo de paisagem voce gosta");
 if(chute== numeroSecreto){
-console.log("boa")
+console.log ("boa com $(tentativa)");
+alert("boa vc descobriu com $(tentativa)");
 }
 else{
 if(numeroSecreto> chute){
-alert(" numero Secreto é maior que ${chute}")
+alert(" numero Secreto é maior que ${chute}");
 
 }else{
-alert("numero secreto é menor que ${chute}")
+alert("numero secreto é menor que ${chute}");
 
 }
-}
+}tentativa++;
 }
